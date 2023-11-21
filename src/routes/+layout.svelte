8ac7bf90246f41ca869098bffa5bc40101647242
Marty Heyn <marty@techslice.com>
2023-11-21 10:38:38 -0500

    gpt SEO suggestions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,24 +44,34 @@
 		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&family=Kanit:wght@400;500&display=swap"
 		rel="stylesheet"
 	/>
-	<meta name="pardon my picks" />
-	<meta name="description" content="Pardon My Take Weekly Football Picks" />
-	<meta name="keywords" content="Pardon My Take, Football, Picks, NFL, Weekly" />
-	<meta name="author" content="Pardon My Picks" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- Page Title -->
+	<title>Pardon My Picks</title>
+
+	<!-- Meta Description -->
+	<meta name="description" content="Weekly football picks by Pardon My Take." />
+
+	<!-- Keywords (Note: Google doesn't rely heavily on meta keywords, but they can still be used) -->
+	<meta name="keywords" content="Pardon My Take, Football Picks, NFL Picks, Weekly Picks" />
+
+	<!-- Canonical URL -->
+	<link rel="canonical" href="https://pardonmypicks.com" />
+
+	<!-- Open Graph Meta Tags (for social media and search engines) -->
 	<meta property="og:url" content="https://pardonmypicks.com" />
-	<meta property="og:title" content="Pardon My Picks" />
-	<meta property="og:description" content="Pardon My Take Weekly Football Picks" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Pardon My Picks - Weekly Football Picks" />
+	<meta property="og:description" content="Weekly football picks by Pardon My Take." />
 	<meta property="og:image" content="https://pardonmypicks.com/og-image.png" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
 	<meta property="og:image:alt" content="Pardon My Picks" />
-	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Pardon My Picks" />
-	<meta property="og:locale" content="en_US" />
-	<meta property="fb:app_id" content="123456789" />
 
-	<title>Pardon My Picks</title>
+	<!-- Twitter Card Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Pardon My Picks - Weekly Football Picks" />
+	<meta name="twitter:description" content="Weekly football picks by Pardon My Take." />
+	<meta name="twitter:image" content="https://pardonmypicks.com/og-image.png" />
 </svelte:head>
 
 <svelte:window bind:innerWidth={$screenWidth} bind:scrollY bind:innerHeight={fullPageHeight} />
