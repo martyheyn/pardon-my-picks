1a30e538f1f9ab939021c0fd6d44b625c4dba846
unknown <heynmarty@gmail.com>
2024-02-29 11:13:36 -0500

    initial bio page ui

---
+++ src/routes/profile/+page.server.ts
@@ -0,0 +1,54 @@
+import { lucia } from '$lib/server/lucia';
+import { prisma } from '$lib/server/prisma';
+import { fail, redirect } from '@sveltejs/kit';
+import { Argon2id } from 'oslo/password';
+
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	if (!locals.user) {
+		throw redirect(303, '/');
+	}
+
+	// make sure it is the right user
+	const existingUser = await prisma.user.findUnique({
+		where: {
+			id: locals.user.id
+		}
+	});
+	if (!existingUser) {
+		throw redirect(303, '/login');
+	}
+
+	return {
+		user: locals.user
+	};
+};
+
+export const actions: Actions = {
+	default: async ({ request, locals }) => {
+		const { user, session } = locals;
+		if (!(user && session)) {
+			// TODO:: throw message to on next screen
+			throw redirect(303, '/login');
+		}
+
+		// get all the user input, or only one input at a time?
+		const formData = await request.formData();
+		const email = formData.get('email');
+		const avatar = formData.get('avatar');
+
+		// TODO:: More of a secruity thought: what if a user an email is used like 100 times?
+		// Should put a limit on how many times an email can be used or how many accounts a machine can create
+
+		const updatedUser = await prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				// email: email,
+				// avatar: avatar
+			}
+		});
+	}
+};
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,3 +1,112 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+	import Icon from '../../lib/components/icon.svelte';
+	import type { PageData } from './$types';
 
-<div class="">Profile Page!</div>
+	export let data: PageData;
+
+	let editting = false;
+	let infoDisplayed = false;
+	let disableSave = false;
+
+	const handleEdit = () => {
+		disableSave = true;
+		// Save changes if editting
+		if (editting) {
+			console.log(`save to db here`);
+		}
+
+		editting = !editting;
+
+		setTimeout(() => {
+			disableSave = false;
+		}, 1000);
+	};
+</script>
+
+<div class="flex flex-col gap-y-4">
+	<div class="border border-black border-opacity-50 w-28 h-28 rounded-full" />
+
+	<div
+		class="max-w-2xl border border-black border-opacity-50 rounded-md px-6 py-4 mt-4 flex flex-col gap-y-4"
+	>
+		<div class="flex">
+			<h2 class="text-xl font-semibold">Profile Details</h2>
+		</div>
+
+		<div class="w-full flex flex-row gap-x-8">
+			<div class="flex-1 flex flex-col justify-between">
+				<label for="username" class="block text-sm font-medium text-gray-600"
+					><span class="pl-1">Username</span>
+				</label>
+
+				{#if editting}
+					<input
+						id="username"
+						name="username"
+						type="text"
+						class="mt-1 py-2 indent-2 border focus:outline-none focus:border-blue-300 w-full rounded-md transition duration-150 ease-in-out"
+						value={data.user.username}
+					/>
+				{:else}
+					<p
+						class="mt-1 bg-stone-50 py-2 indent-2 border focus:outline-none focus:border-blue-300 w-full rounded-md transition duration-150 ease-in-out"
+					>
+						{data.user.username}
+					</p>
+				{/if}
+			</div>
+
+			<div class="flex-1">
+				<label for="email" class="block text-sm font-medium text-gray-600">
+					<div class="flex justify-between items-center pr-2">
+						<span class="pl-1">Email</span>
+						<button on:click={() => (infoDisplayed = !infoDisplayed)}>
+							<Icon
+								class={`transition-all duration-300 ease-in-out cursor-pointer rounded-full  hover:scale-110 `}
+								fillRule="evenodd"
+								clipRule="evenodd"
+								width="16px"
+								height="16px"
+								iconName="info"
+							/>
+						</button>
+					</div>
+				</label>
+				{#if infoDisplayed}
+					<p
+						class="text-xs py-1 px-[2px] text-slate-600"
+						transition:slide={{ duration: 300, delay: 100 }}
+					>
+						In case you forget your password and want to reset it, please enter email. Otherwise,
+						chill
+					</p>
+				{/if}
+
+				{#if editting}
+					<input
+						id="email"
+						name="email"
+						type="email"
+						class="mt-1 py-2 indent-2 border focus:outline-none focus:border-blue-300 w-full rounded-md transition duration-150 ease-in-out"
+					/>
+				{:else}
+					<p
+						class="mt-1 bg-stone-50 py-2 indent-2 border focus:outline-none focus:border-blue-300 w-full rounded-md transition duration-150 ease-in-out"
+					>
+						Test Email
+					</p>
+				{/if}
+			</div>
+		</div>
+
+		<button
+			on:click={handleEdit}
+			disabled={disableSave}
+			class={`w-fit text-white ${
+				disableSave ? 'bg-gray-400' : 'bg-primary hover:bg-primaryHover'
+			} border rounded-md px-4 py-1.5 transition-all duration-200 ease-in-out`}
+			>{editting ? 'Save Changes' : 'Edit Profile'}</button
+		>
+	</div>
+</div>
