e47bfecc65d76ef7d88e42c99b16da8e0c867ccb
unknown <heynmarty@gmail.com>
2024-03-20 12:53:31 -0400

    migrated to neon, updated stats raw sql query to postgres

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -2,7 +2,11 @@
 // SvelteKit hot reloads on all changes and we do want to create new Prisma Client on every reload
 import { PrismaClient } from '@prisma/client';
 
-const prisma = globalThis.prisma || new PrismaClient();
+const prisma =
+	globalThis.prisma ||
+	new PrismaClient({
+		log: ['query']
+	});
 
 // if in development, set global.prisma to the newly created Prisma Client
 if (process.env.NODE_ENV === 'development') globalThis.prisma = prisma;
--- src/routes/stats/+page.server.ts
+++ src/routes/stats/+page.server.ts
@@ -39,16 +39,18 @@ const getRecordPct = (total_picks: number, wins: number, pushes: number) => {
 export const load: PageServerLoad = async () => {
 	// get all of the tail and fade data by person
 	const rawPersonData: RawPersonData[] = await prisma.$queryRaw`
-	SELECT p.person, COUNT(p.winner) as total_picks,
-	SUM(p.winner) as wins, SUM(p.push) as pushes,
-	COUNT(t.id) as total_tails,
-	SUM(CASE WHEN t.winner THEN 1 ELSE 0 END) as tail_wins,
-	COUNT(f.id) as total_fades,
-	SUM(CASE WHEN f.winner THEN 1 ELSE 0 END) as fade_wins
-	FROM Pick as p
-	LEFT JOIN Tail as t ON p.id = t.pickId
-	LEFT JOIN Fade as f ON p.id = f.pickId
-	GROUP BY p.person
+	SELECT p.person, 
+       COUNT(p.winner) as total_picks,
+       SUM(p.winner) as wins, 
+       SUM(p.push) as pushes,
+       COUNT(t.id) as total_tails,
+       SUM(CASE WHEN t.winner THEN 1 ELSE 0 END) as tail_wins,
+       COUNT(f.id) as total_fades,
+       SUM(CASE WHEN f.winner THEN 1 ELSE 0 END) as fade_wins
+	   FROM "Pick" as p
+	   LEFT JOIN "Tail" as t ON p.id = t."pickId"
+	   LEFT JOIN "Fade" as f ON p.id = f."pickId"
+	   GROUP BY p.person;
 	`;
 
 	// console.log('rawPersonData', rawPersonData);
@@ -56,16 +58,20 @@ export const load: PageServerLoad = async () => {
 	rawPersonData.map((person) => {
 		personData.push({
 			person: person.person,
-			wins: person.wins.toString(),
-			pushes: person.pushes.toString(),
-			record: `${person.wins} - ${Number(person.total_picks) - person.wins - person.pushes} - ${
-				person.pushes
-			}`,
-			record_pct: getRecordPct(person.total_picks, person.wins, person.pushes).toString(),
-			total_tails: person.total_tails.toString(),
-			tails_pct: ((person.tail_wins / Number(person.total_tails)) * 100).toString(),
-			total_fades: person.total_fades.toString(),
-			fades_pct: ((person.fade_wins / Number(person.total_fades)) * 100).toString()
+			wins: Number(person.wins).toString(),
+			pushes: Number(person.pushes).toString(),
+			record: `${person.wins} - ${
+				Number(person.total_picks) - Number(person.wins) - Number(person.pushes)
+			} - ${Number(person.pushes)}`,
+			record_pct: getRecordPct(
+				Number(person.total_picks),
+				Number(person.wins),
+				Number(person.pushes)
+			).toString(),
+			total_tails: Number(person.total_tails).toString(),
+			tails_pct: ((Number(person.tail_wins) / Number(person.total_tails)) * 100).toString(),
+			total_fades: Number(person.total_fades).toString(),
+			fades_pct: ((Number(person.fade_wins) / Number(person.total_fades)) * 100).toString()
 		});
 	});
 
