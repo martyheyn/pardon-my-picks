165dbc0e65e1a2902beec9834d85ac079d2f0b58
Marty Heyn <heynmarty@gmai.com>
2024-10-04 07:00:18 -0400

    added week 5 picks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 	import type { Page } from '@sveltejs/kit';
 
 	const sideNavCollasped = writable(true);
-	const currWeek = writable(4);
+	const currWeek = writable(5);
 	const currYear = writable(2024);
 	const screenWidth = writable(0);
 	const active = writable('');
